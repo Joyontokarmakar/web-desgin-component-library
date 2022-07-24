a8abdfcd1531cd044104d47f8263500948f5d7c9
Joyonto Karmakar <joyonto1997@gmail.com>
2022-07-25 00:08:42 +0600

    side bar profile section re-design done

--- components/sidebars/index.vue
+++ components/sidebars/index.vue
@@ -1,10 +1,9 @@
 <template>
   <div class="hidden md:block w-[300px] p-5 bg-corporateBlack h-screen divide-y divide-corporateGray">
-    <div class="py-3">
-      <img src="@/assets/images/logo/webDesignComponentLibrary-Logo.png" alt="profile" class="rounded-full w-2/5 shadow-md mx-auto"/>
-      <div class="leading-tight">
-        <h2 class="uppercase font-bold text-xl text-center  text-corporateGray">Joyonto Karmakar</h2>
-        <h2 class="text-sm text-center text-corporateGray">admin@domain.com</h2>
+    <div class="py-3 flex justify-start items-center gap-5">
+      <img src="@/assets/images/logo/webDesignComponentLibrary-Logo.png" alt="profile" class="rounded-full w-1/5 shadow-md mx-auto"/>
+      <div class="leading-6 ">
+        <h2 class="uppercase font-bold text-sm text-corporateGray">Web Design & Components Library</h2>
       </div>
     </div>
     <div class="mt-5 space-y-2 overflow-y-auto h-[calc(100vh-285px)] py-2">
