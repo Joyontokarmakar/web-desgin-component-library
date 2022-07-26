343c18dcb584d9e2a80d2abfafeb4d15f4bc761a
Joyonto Karmakar <joyonto1997@gmail.com>
2022-07-27 01:00:25 +0600

    code copy done

--- components/sourceView/code/index.vue
+++ components/sourceView/code/index.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="bg-corporateBlack text-corporateGray border rounded w-full overflow-x-auto">
+  <div class="group relative bg-corporateBlack text-corporateGray border rounded w-full overflow-x-auto">
     <!-- <pre class="language-html">
       <code>
         {{props.mainCode}}
       </code>
     </pre> -->
+    <button @click="copyText" class="py-1 px-2 border-0 rounded text-sm bg-corporate text-white shadow-sm transition ease-in duration-300 hover:scale-125 hidden group-hover:block group-hover:absolute group-hover:top-2 group-hover:right-2">Copy</button>
+    <div class="bg-corporateGray text-corporateLight rounded px-2 " :class="tooltip ? 'block absolute top-12 right-2': 'hidden'"><span>Copied !</span></div>
     <xmp>
       {{props.mainCode}}
     </xmp>
@@ -13,6 +15,13 @@
 
 <script setup>
   const props = defineProps({mainCode: { type: String,}})
+  const tooltip = ref(false);
+  const copyText = () =>{
+    const  data = navigator.clipboard.writeText(props.mainCode);
+    setTimeout(() => tooltip.value = true, 0);
+    setTimeout(() => tooltip.value = false, 1000);
+    return data;
+  }
 </script>
 
 <style></style>
