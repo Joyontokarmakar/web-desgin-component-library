6320601b18724892b4c36a86551f3263eb6c0f77
Joyonto Karmakar <joyonto1997@gmail.com>
2022-07-30 23:32:24 +0600

    mainmenubutton issue solve

--- components/sidebarMainmenuButton/index.vue
+++ components/sidebarMainmenuButton/index.vue
@@ -2,8 +2,8 @@
      <NuxtLink
           :to="props.pathName"
           class="mainMenuClass flex justify-start items-center gap-x-2 ml-4"
-          :class="$route.path === props.pathName ? 'mainMenuActiveClass' : ''"
-          >
+          :class="$route.path === props.pathName ? 'mainMenuActiveClass' : 
+               props.subMenu ? '' : ''">
           <slot/>
           <h5 class="mainMenuClass mb-0" :class="$route.path === props.pathName ? 'mainMenuActiveClass' : ''">
                {{props.name}}
@@ -12,11 +12,11 @@
 </template>
 
 <script setup>
-const props = defineProps({
-     pathName: { type: String,}, 
-     name: {type: String}, 
-     subMenu: {type: Boolean}
-})
+     const props = defineProps({
+          pathName: { type: String,}, 
+          name: {type: String}, 
+          subMenu: {type: Boolean}
+     })
 </script>
 
 <style></style>
