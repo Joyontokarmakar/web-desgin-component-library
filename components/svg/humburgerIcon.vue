39704d1d855b224e65e1d38ac58440158df1dff9
Joyonto Karmakar <joyonto1997@gmail.com>
2022-08-08 23:48:35 +0600

    search, ok, humburgermenuIcom component created

---
+++ components/svg/humburgerIcon.vue
@@ -0,0 +1,34 @@
+<template>
+  <svg 
+    :fill="props.fill" 
+    xmlns="http://www.w3.org/2000/svg" 
+    viewBox="0 0 24 24" 
+    :width="props.width"
+    :height="props.height"
+    >    
+      <path 
+        d="M 2 5 L 2 7 L 22 7 L 22 5 L 2 5 z M 2 11 L 2 13 L 22 13 L 22 11 L 2 11 z M 2 17 L 2 19 L 22 19 L 22 17 L 2 17 z"
+        :fill="props.fill"
+        :stroke="props.fill"
+      />
+  </svg>
+</template>
+
+<script setup>
+  const props = defineProps({
+    width: {
+      type: String,
+      default: "26",
+    },
+    height: {
+      type: String,
+      default: "26",
+    },
+    fill: {
+      type: String,
+      default: "#DCDCDC",
+    },
+  })
+</script>
+
+<style></style>
---
+++ components/svg/okIcon.vue
@@ -0,0 +1,34 @@
+<template>
+  <svg 
+    :fill="props.fill" 
+    xmlns="http://www.w3.org/2000/svg" 
+    viewBox="0 0 24 24" 
+    :width="props.width"
+    :height="props.height"
+    >    
+      <path 
+        d="M 20.292969 5.2929688 L 9 16.585938 L 4.7070312 12.292969 L 3.2929688 13.707031 L 9 19.414062 L 21.707031 6.7070312 L 20.292969 5.2929688 z"
+        :fill="props.fill"
+        :stroke="props.fill"
+      />
+  </svg>
+</template>
+
+<script setup>
+  const props = defineProps({
+    width: {
+      type: String,
+      default: "26",
+    },
+    height: {
+      type: String,
+      default: "26",
+    },
+    fill: {
+      type: String,
+      default: "#DCDCDC",
+    },
+  })
+</script>
+
+<style></style>
