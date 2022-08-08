4388d411765685d38ae5c8db1712f3dcdac429fa
Joyonto Karmakar <joyonto1997@gmail.com>
2022-08-08 23:42:34 +0600

    puzel, close, document, edit icon component created

---
+++ components/svg/closeIcon.vue
@@ -0,0 +1,34 @@
+<template>
+  <svg 
+    :fill="props.fill" 
+    xmlns="http://www.w3.org/2000/svg" 
+    viewBox="0 0 24 24" 
+    :width="props.width"
+    :height="props.height"
+    >    
+      <path 
+        d="M 4.7070312 3.2929688 L 3.2929688 4.7070312 L 10.585938 12 L 3.2929688 19.292969 L 4.7070312 20.707031 L 12 13.414062 L 19.292969 20.707031 L 20.707031 19.292969 L 13.414062 12 L 20.707031 4.7070312 L 19.292969 3.2929688 L 12 10.585938 L 4.7070312 3.2929688 z"
+        :fill="props.fill"
+        :stroke="props.fill"
+      />
+  </svg>
+</template>
+
+<script setup>
+  const props = defineProps({
+    width: {
+      type: String,
+      default: "26",
+    },
+    height: {
+      type: String,
+      default: "26",
+    },
+    fill: {
+      type: String,
+      default: "#DCDCDC",
+    },
+  })
+</script>
+
+<style></style>
---
+++ components/svg/documentIcon.vue
@@ -0,0 +1,34 @@
+<template>
+  <svg 
+    :fill="props.fill" 
+    xmlns="http://www.w3.org/2000/svg" 
+    viewBox="0 0 24 24" 
+    :width="props.width"
+    :height="props.height"
+    >    
+      <path 
+        d="M 6 2 C 4.9057453 2 4 2.9057453 4 4 L 4 20 C 4 21.094255 4.9057453 22 6 22 L 18 22 C 19.094255 22 20 21.094255 20 20 L 20 8 L 14 2 L 6 2 z M 6 4 L 13 4 L 13 9 L 18 9 L 18 20 L 6 20 L 6 4 z M 8 12 L 8 14 L 16 14 L 16 12 L 8 12 z M 8 16 L 8 18 L 16 18 L 16 16 L 8 16 z"
+        :fill="props.fill"
+        :stroke="props.fill"
+      />
+  </svg>
+</template>
+
+<script setup>
+  const props = defineProps({
+    width: {
+      type: String,
+      default: "26",
+    },
+    height: {
+      type: String,
+      default: "26",
+    },
+    fill: {
+      type: String,
+      default: "#DCDCDC",
+    },
+  })
+</script>
+
+<style></style>
